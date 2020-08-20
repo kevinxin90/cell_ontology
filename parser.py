73aa1227f206a080f8e872a5dd9ab48bd7f74c95
jiwen xin <kevinxin@scripps.edu>
2020-08-20 09:34:06 -0700

    fix synonym and ancestor

--- parser.py
+++ parser.py
@@ -2,6 +2,38 @@ from collections import defaultdict
 import networkx as nx
 import obonet
 import os
+import re
+
+
+def get_synonyms(data):
+    """Format synonyms as dicionary
+    exact and related synonyms are the keys, and their values are in lists
+    """
+    if 'synonym' in data:
+        syn_dict = {}
+        exact = []
+        related = []
+        broad = []
+        for syn in data['synonym']:
+            if 'EXACT' in syn:
+                match = re.findall(r'\"(.+?)\"', syn)
+                exact = exact + match
+            elif 'RELATED' in syn:
+                match = re.findall(r'\"(.+?)\"', syn)
+                related = related + match
+            elif 'BROAD' in syn:
+                match = re.findall(r'\"(.+?)\"', syn)
+                broad = broad + match
+        synonyms = {}
+        if len(exact) > 0:
+            synonyms["exact"] = exact
+        if len(related) > 0:
+            synonyms["related"] = related
+        if len(broad) > 0:
+            synonyms["broad"] = broad
+        return synonyms
+    else:
+        return {}
 
 
 def load_data(data_folder):
@@ -13,7 +45,7 @@ def load_data(data_folder):
             rec["_id"] = item
             if rec.get("is_a"):
                 rec["parents"] = [parent for parent in rec.pop(
-                    "is_a") if parent.startswith("UBERON:")]
+                    "is_a") if parent.startswith("CL:")]
             if rec.get("xref"):
                 xrefs = defaultdict(set)
                 for val in rec.get("xref"):
@@ -30,11 +62,12 @@ def load_data(data_folder):
                 rec.pop("xref")
                 rec["xrefs"] = dict(xrefs)
             rec["children"] = [child for child in graph.predecessors(
-                item) if child.startswith("UBERON:")]
+                item) if child.startswith("CL:")]
             rec["ancestors"] = [ancestor for ancestor in nx.descendants(
-                graph, item) if ancestor.startswith("UBERON:")]
+                graph, item) if ancestor.startswith("CL:")]
             rec["descendants"] = [descendant for descendant in nx.ancestors(
-                graph, item) if descendant.startswith("UBERON:")]
+                graph, item) if descendant.startswith("CL:")]
+            rec["synonym"] = get_synonyms(rec)
             if rec.get("created_by"):
                 rec.pop("created_by")
             if rec.get("creation_date"):
